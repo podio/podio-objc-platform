6f321676685141bc83143034a3905bc08e905d73
Sebastian Rehnby <sebastian@rehnby.com>
2015-02-15 21:47:30 +0100

    Added framework target

--- PodioPlatformKit/TestClass.h
+++ PodioPlatformKit/TestClass.h
@@ -1,9 +1,13 @@
 //
-//  PodioPlatformKit.h
+//  TestClass.h
 //  PodioPlatformKit
 //
 //  Created by Sebastian Rehnby on 15/02/15.
 //  Copyright (c) 2015 Citrix Systems, Inc. All rights reserved.
 //
 
-#import "PodioKitCore.h"
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
--- PodioPlatformKit/TestClass.m
+++ PodioPlatformKit/TestClass.m
@@ -1,9 +1,13 @@
 //
-//  PodioPlatformKit.h
+//  TestClass.m
 //  PodioPlatformKit
 //
 //  Created by Sebastian Rehnby on 15/02/15.
 //  Copyright (c) 2015 Citrix Systems, Inc. All rights reserved.
 //
 
-#import "PodioKitCore.h"
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
